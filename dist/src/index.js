a6623521e5914e6fd35d968cfb3a9f5ac7e1ffcf
Anatoly Novoselov <iseekyouu@yandex.ru>
2023-02-09 12:04:10 +0700

    added dist

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseConsumer = void 0;
+exports.BaseProducer = exports.BaseConsumer = void 0;
 const base_consumer_1 = require("./base.consumer");
 Object.defineProperty(exports, "BaseConsumer", { enumerable: true, get: function () { return base_consumer_1.BaseConsumer; } });
+const base_producer_1 = require("./base.producer");
+Object.defineProperty(exports, "BaseProducer", { enumerable: true, get: function () { return base_producer_1.BaseProducer; } });
