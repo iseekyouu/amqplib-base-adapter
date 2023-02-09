eae1494bb7084d2fef6a45063b8fad173f3d7329
vylor <shibaev.m@vlprojects.pro>
2023-02-09 15:22:11 +0500

    fix readme

--- examples/config.ts
+++ examples/config.ts
@@ -1,5 +1,5 @@
 export const env = {
-  RMQ_CLUSTER_ADDRESSES: 'rabbitmq',
+  RMQ_CLUSTER_ADDRESS: 'rabbitmq',
   RMQ_CLUSTER_PASSWORD: 'rabbitmq',
   RMQ_CLUSTER_PORT: 5672,
   RMQ_CLUSTER_USERNAME: 'rabbitmq',
